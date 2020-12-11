d13935ed2acee4c8b15f459a6f2ca5a75156c942
Gwalker24 <74081444+Gwalker24@users.noreply.github.com>
2020-12-11 11:20:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Gianna Walker
+Period/Core: 6 
 
 
 """
+color = input("Enter a color: ")
+verbb = input("Enter a Verb: ")
+food = input("Enter a Food: ")
+name = input("Enter a Name: ")
+celebrity = input("Enter a Celebrity name: ")
+animal = input("Enter an Animal: ")
+typeofchip = input("Enter a Flavor of chip: ")
+jollyrancherflavor = input("Enter your favorite Jolly Rancher Flavor: ")
+
+
+
+
+print("The sky was", color)
+print(verbb + " to the bus stop")
+print("I stop to pick up", Food)
+print(name + "met me at school.")
+print(celebrity + " was there and they talked to our school about bullying. It was boring until they satrted to sing Twinkle Twinkle Little Star.")
+print("On the way home my friend and I saw", animal)
+print("It looked hungry, I gave it my favorite chip,", typeofchip)
+print(jollyrancherflavor + "is my favorie flavor of Jolly Ranchers and while walking in the front door my mom handed me a bag, with 500 of them!")
+print("Today was such a weird day. I had a lot of fun and now I have a lot of jolly ranchers to eat.")
+
